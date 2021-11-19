2532f4e2f3a064a40b50b634d29d1ee6b5f8c04c
Sadek80 <mohamedsadk80@gmail.com>
2021-11-19 16:09:47 +0200

    "Edits Validtaion attribute of course"

--- ValidationAttributes/CourseTitleMustBeDifferentFromDescriptionAttribute.cs
+++ ValidationAttributes/CourseTitleMustBeDifferentFromDescriptionAttribute.cs
@@ -12,12 +12,28 @@ namespace CourseLibrary.Api.ValidationAttributes
     {
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
-            var course = (CourseForManipulationDto) validationContext.ObjectInstance;
+            var objectInstance = validationContext.ObjectInstance;
+            var course = validationContext.ObjectInstance;
 
-            if(course.Title == course.Description)
+            if (course is IEnumerable<CourseForManipulationDto>)
             {
-                return new ValidationResult(ErrorMessage,
-                    new[] { "CourseForManipulationDto" });
+                foreach (var c in course as IEnumerable<CourseForManipulationDto>)
+                {
+                    if (c.Title == c.Description)
+                    {
+                        return new ValidationResult(ErrorMessage,
+                            new[] { "CourseForManipulationDto" });
+                    }
+                }
+            }
+            else
+            {
+                var singleCourse = (CourseForManipulationDto) course;
+                if (singleCourse.Title == singleCourse.Description)
+                {
+                    return new ValidationResult(ErrorMessage,
+                        new[] { "CourseForManipulationDto" });
+                }
             }
 
             return ValidationResult.Success;
